b3d2dc763b54a5f67650d229d94bca3e6a97743b
dusanvin <vincent.dusanek@gmail.com>
2025-03-27 10:53:06 +0100

    Responsive Deisgn Nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,12 +15,11 @@
             </a>
             <div class="flex items-center">
                 @auth
-                    <svg class="text-gray-300 mr-2" width="48" height="48" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <svg class="text-gray-300 mr-2 hidden lg:block" width="48" height="48" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                         <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
                     </svg>
                     <div class="flex flex-col items-start justify-center h-full">
                         <a href="{{ route('profile.edit') }}" class="text-gray-300 font-semibold text-sm hover:text-white mb-0">{{ Auth::user()->name }}</a>
-
                         <div class="text-gray-300 text-xs">
                             <!-- Hier können Sie den zusätzlichen Text hinzufügen -->
                             {{ Auth::user()->email }}
@@ -33,7 +32,7 @@
                             <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2">
                                 <path fill-rule="evenodd" d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
                             </svg>
-                            Ausloggen
+                            <span class="hidden lg:block">Ausloggen</span>
                         </button>
                     </form>
                 @endauth
@@ -43,19 +42,19 @@
     <nav class="bg-gray-600">
         <div class="lg:container mx-auto flex items-center">
             @auth
-                <a href="{{ route('devices.overview') }}" class="rounded-t p-4 text-sm mr-2 flex items-center hover:text-white {{ Route::currentRouteName() == 'devices.overview' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2">
+                <a href="{{ route('devices.overview') }}" class="p-4 ml-4 lg:ml-0 m-0 md:mr-2 md:p-4 rounded-t text-sm  flex items-center hover:text-white {{ Route::currentRouteName() == 'devices.overview' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-0 lg:mr-2">
                         <path fill-rule="evenodd" d="M5.478 5.559A1.5 1.5 0 0 1 6.912 4.5H9A.75.75 0 0 0 9 3H6.912a3 3 0 0 0-2.868 2.118l-2.411 7.838a3 3 0 0 0-.133.882V18a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0 0 17.088 3H15a.75.75 0 0 0 0 1.5h2.088a1.5 1.5 0 0 1 1.434 1.059l2.213 7.191H17.89a3 3 0 0 0-2.684 1.658l-.256.513a1.5 1.5 0 0 1-1.342.829h-3.218a1.5 1.5 0 0 1-1.342-.83l-.256-.512a3 3 0 0 0-2.684-1.658H3.265l2.213-7.191Z" clip-rule="evenodd" />
                         <path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v6.44l1.72-1.72a.75.75 0 1 1 1.06 1.06l-3 3a.75.75 0 0 1-1.06 0l-3-3a.75.75 0 0 1 1.06-1.06l1.72 1.72V3a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
                       </svg>
-                    Ausgeliehene Geräte
+                    <span class="hidden lg:block">Ausgeliehene Geräte</span> 
                 </a>
-                <a href="{{ route('devices.index') }}" class="rounded-t p-4 text-sm mr-14 flex items-center hover:text-white {{ Route::currentRouteName() == 'devices.index' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2">
+                <a href="{{ route('devices.index') }}" class="p-4 mr-0 lg:mr-14 rounded-t text-sm flex items-center hover:text-white {{ Route::currentRouteName() == 'devices.index' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-0 lg:mr-2">
                         <path fill-rule="evenodd" d="M1.5 9.832v1.793c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875V9.832a3 3 0 0 0-.722-1.952l-3.285-3.832A3 3 0 0 0 16.215 3h-8.43a3 3 0 0 0-2.278 1.048L2.222 7.88A3 3 0 0 0 1.5 9.832ZM7.785 4.5a1.5 1.5 0 0 0-1.139.524L3.881 8.25h3.165a3 3 0 0 1 2.496 1.336l.164.246a1.5 1.5 0 0 0 1.248.668h2.092a1.5 1.5 0 0 0 1.248-.668l.164-.246a3 3 0 0 1 2.496-1.336h3.165l-2.765-3.226a1.5 1.5 0 0 0-1.139-.524h-8.43Z" clip-rule="evenodd" />
                         <path d="M2.813 15c-.725 0-1.313.588-1.313 1.313V18a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-1.688c0-.724-.588-1.312-1.313-1.312h-4.233a3 3 0 0 0-2.496 1.336l-.164.246a1.5 1.5 0 0 1-1.248.668h-2.092a1.5 1.5 0 0 1-1.248-.668l-.164-.246A3 3 0 0 0 7.046 15H2.812Z" />
                     </svg>
-                    Geräte
+                    <span class="hidden lg:block">Geräte</span>
                 </a>
               <!-- <a href="{{ route('devices.create') }}" class="rounded-t p-4 text-sm mr-14 flex items-center hover:text-white {{ Route::currentRouteName() == 'devices.create' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
                     <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2">
@@ -63,32 +62,32 @@
                     </svg>
                     Gerät hinzufügen
                 </a> -->
-                <a href="{{ route('reservations.index') }}" class="rounded-t p-4 text-sm mr-2 flex items-center hover:text-white {{ Route::currentRouteName() == 'reservations.index' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2">
+                <a href="{{ route('reservations.index') }}" class="p-4 mr-0 lg:mr-2 rounded-t text-sm flex items-center hover:text-white {{ Route::currentRouteName() == 'reservations.index' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-0 lg:mr-2">
                         <path fill-rule="evenodd" d="M4.125 3C3.089 3 2.25 3.84 2.25 4.875V18a3 3 0 0 0 3 3h15a3 3 0 0 1-3-3V4.875C17.25 3.839 16.41 3 15.375 3H4.125ZM12 9.75a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H12Zm-.75-2.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5H12a.75.75 0 0 1-.75-.75ZM6 12.75a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5H6Zm-.75 3.75a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5H6a.75.75 0 0 1-.75-.75ZM6 6.75a.75.75 0 0 0-.75.75v3c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-3A.75.75 0 0 0 9 6.75H6Z" clip-rule="evenodd" />
                         <path d="M18.75 6.75h1.875c.621 0 1.125.504 1.125 1.125V18a1.5 1.5 0 0 1-3 0V6.75Z" />
                       </svg>
-                    Gebuchte Räume
+                      <span class="hidden lg:block">Gebuchte Räume</span>
                 </a>                
-                <a href="{{ route('rooms.index') }}" class="rounded-t p-4 text-sm mr-14 flex items-center hover:text-white {{ Route::currentRouteName() == 'rooms.index' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2">
+                <a href="{{ route('rooms.index') }}" class="p-4 mr-0 lg:mr-14 rounded-t text-sm flex items-center hover:text-white {{ Route::currentRouteName() == 'rooms.index' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-0 lg:mr-2">
                         <path fill-rule="evenodd" d="M7.5 3.75A1.5 1.5 0 0 0 6 5.25v13.5a1.5 1.5 0 0 0 1.5 1.5h6a1.5 1.5 0 0 0 1.5-1.5V15a.75.75 0 0 1 1.5 0v3.75a3 3 0 0 1-3 3h-6a3 3 0 0 1-3-3V5.25a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3V9A.75.75 0 0 1 15 9V5.25a1.5 1.5 0 0 0-1.5-1.5h-6Zm5.03 4.72a.75.75 0 0 1 0 1.06l-1.72 1.72h10.94a.75.75 0 0 1 0 1.5H10.81l1.72 1.72a.75.75 0 1 1-1.06 1.06l-3-3a.75.75 0 0 1 0-1.06l3-3a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
                     </svg>
-                    Räume
+                    <span class="hidden lg:block">Räume</span>
                 </a>
                 @if(Auth::user()->role == 'administration')
                     <a href="{{ route('users.index') }}" class="rounded-t p-4 text-sm mr-2 flex items-center hover:text-white {{ Route::currentRouteName() == 'users.index' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
-                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-3">
+                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-0 lg:mr-3">
                             <path fill-rule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clip-rule="evenodd" />
                             <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
                           </svg>
-                        Konten
+                          <span class="hidden lg:block">Konten</span>
                     </a>
                     <a href="{{ route('users.create') }}" class="rounded-t p-4 text-sm mr-12 flex items-center hover:text-white {{ Route::currentRouteName() == 'users.create' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
-                          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-3">
+                          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-0 lg:mr-3">
                             <path d="M5.25 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM2.25 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM18.75 7.5a.75.75 0 0 0-1.5 0v2.25H15a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H21a.75.75 0 0 0 0-1.5h-2.25V7.5Z" />
                           </svg>
-                        Konto hinzufügen
+                          <span class="hidden lg:block">Konto hinzufügen</span>
                     </a>
                     <!-- <a href="{{ route('devices.create') }}" class="rounded-t p-4 text-sm mr-12 flex items-center hover:text-white {{ Route::currentRouteName() == 'devices.edit' ? 'text-gray-800 bg-white hover:text-gray-800' : 'text-gray-300 ' }}">
                         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-3">
